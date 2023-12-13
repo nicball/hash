01781b185a5e137ba96054d099122fee0321b990
Nick Ballard <znhihgiasy@gmail.com>
2023-12-13 10:56:34 +0800

    For the sake of my sanity, just ignore this...

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
             version = "0.1.0";
             src = ./.;
             buildInputs = [ (haskellPackages.ghcWithPackages (p: with p; [
-              process bytestring utf8-string
+              process bytestring utf8-string unix
             ]) ) ];
             buildPhase = ''
               runHook preBuild
--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,18 @@ import Data.List (singleton)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.UTF8 as ByteString hiding (length)
 import qualified Data.ByteString.Lazy as ByteString
+import qualified Data.ByteString.Lazy.Char8 as ByteString (unlines, putStrLn)
 import qualified System.Process as Process
 import Control.Exception (Exception, throwIO)
 import Control.Concurrent (forkIO)
 import Control.Monad (when)
 import System.IO (hClose)
+
+infix 2 .$
+infixl 3 .>
+infixl 3 .<
+infixr 4 .|
+infixr 4 `Pipe`
 
 data Command a b where
   PureCommand :: (a -> IO b) -> Command a b
@@ -48,17 +55,11 @@ class Adaptable a b where
 instance Adaptable a a where
   adapt = id
 
--- instance Show a => Adaptable a String where
---   adapt = show
-
 instance Adaptable a [a] where
   adapt = singleton
 
 instance Adaptable a (Maybe a) where
   adapt = Just
-
-instance Adaptable ByteString [ByteString] where
-  adapt = ByteString.lines
 
 instance Adaptable ByteString () where
   adapt = const ()
@@ -66,8 +67,26 @@ instance Adaptable ByteString () where
 instance Adaptable () ByteString where
   adapt = const ByteString.empty
 
-class CommandShow a where
-  commandShow :: a -> String
+instance Adaptable a ByteString => Adaptable a [ByteString] where
+  adapt = ByteString.lines . adapt
+
+instance Adaptable a ByteString => Adaptable [a] ByteString where
+  adapt = ByteString.unlines . map adapt
+
+class ToScreen a where
+  toScreen :: a -> ByteString
+
+instance ToScreen ByteString where
+  toScreen = id
+
+instance ToScreen a => ToScreen [a] where
+  toScreen = ByteString.unlines . map toScreen
+
+instance ToScreen () where
+  toScreen = const ByteString.empty
+
+instance ToScreen Int where
+  toScreen = ByteString.fromString . show
 
 (.|) :: Adaptable b c => Command a b -> Command c d -> Command a d
 (.|) x y = x `Pipe` PureCommand (pure . adapt) `Pipe` y
@@ -93,7 +112,9 @@ hs = PureCommand
 retcode :: String -> Command ByteString Int
 retcode cmd = ShellCommand ("{ " ++ cmd ++ "; } > /dev/null; echo $?") .| hs (pure . read . ByteString.toString)
 
-(.$) :: Adaptable c a => Command a b -> c -> IO b
-f .$ a = runCommand f (adapt a)
+(.$) :: (Adaptable c a, ToScreen b) => Command a b -> c -> IO ()
+f .$ a = ByteString.putStrLn =<< toScreen <$> runCommand f (adapt a)
 
-
+-- data FileSpec = FileSpec
+--   { fileName :: String
+--   , file
